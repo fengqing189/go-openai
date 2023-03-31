f75659ae581d3d14e50e70cfa68ebf56276bdbfa
bfq <xyb2938@dingtalk.com>
2023-03-31 10:09:20 +0800

    init

--- config.go
+++ config.go
@@ -31,3 +31,14 @@ func DefaultConfig(authToken string) ClientConfig {
 		EmptyMessagesLimit: defaultEmptyMessagesLimit,
 	}
 }
+
+func MyConfig(authToken string, apiURL string) ClientConfig {
+	return ClientConfig{
+		HTTPClient: &http.Client{},
+		BaseURL:    apiURL,
+		OrgID:      "",
+		authToken:  authToken,
+
+		EmptyMessagesLimit: defaultEmptyMessagesLimit,
+	}
+}
